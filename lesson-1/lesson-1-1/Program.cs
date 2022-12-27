b8981066d40cd08af02b190a2eb3784f95e23020
Камила Боровикова <kamale1792@mail.ru>
2022-12-27 10:21:45 +0300

    задания по лекции 9

---
+++ lesson-1/lesson-1-1/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// M = 5; N = 3 -> "5, 4, 3"
+// M = 8; N = 4. -> "8, 7, 6, 5, 4"
+
+Console.WriteLine("Введите натуральное число больше 1:");
+int number = int.Parse(Console.ReadLine());
+
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    NumberCounter (number - 1);
+}
+
+NumberCounter(number);
